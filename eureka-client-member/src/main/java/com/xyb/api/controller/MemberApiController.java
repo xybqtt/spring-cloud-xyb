0f4264a7b33300bc216a8f8fdb8894e80e69fcd6
xybqtt <13572180490@163.com>
2019-03-12 07:46:50 +0800

    eureka2

--- eureka-client-member/src/main/java/com/xyb/api/controller/MemberApiController.java
+++ eureka-client-member/src/main/java/com/xyb/api/controller/MemberApiController.java
@@ -1,14 +1,18 @@
 package com.xyb.api.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberApiController {
 
+    @Value("${server.port}")
+    private String servicePort;
+
     @RequestMapping("/getMember")
     public String getOrder(){
-        return "this is member";
+        return "this is member，端口号：" + servicePort;
     }
 }
--- eureka-client-order/src/main/java/com/xyb/EurekaClientOrderApplication.java
+++ eureka-client-order/src/main/java/com/xyb/EurekaClientOrderApplication.java
@@ -2,7 +2,10 @@ package com.xyb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +15,13 @@ public class EurekaClientOrderApplication {
         SpringApplication.run(EurekaClientOrderApplication.class, args);
     }
 
+    @Bean
+    /**
+     * 以http://ip:port/getMember调用不需要@LoadBalanced注解
+     * 以http://服务别名:port/getMember调用需要添加，且有负载均衡的作用
+     */
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
